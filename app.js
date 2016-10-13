7a4904ad277a109e0d04f9a99b26bdaa161b5eeb
reazy015 <redshuhart015@gmail.com>
2016-10-13 21:53:23 +0300

    add slogan and font-style

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const Server = require('./server.js')
-const port = (process.env.PORT || 8080)
-const app = Server.app()
+import React from 'react';
+import Menu from './Menu';
 
-if (process.env.NODE_ENV !== 'production') {
-    const webpack = require('webpack')
-    const webpackDevMiddleware = require('webpack-dev-middleware')
-    const webpackHotMiddleware = require('webpack-hot-middleware')
-    const config = require('./webpack.dev.config.js')
-    const compiler = webpack(config)
-  
-      app.use(webpackHotMiddleware(compiler))
-      app.use(webpackDevMiddleware(compiler, {
-      noInfo: true,
-      publicPath: config.output.publicPath
-    }))
+export default class App extends React.Component {	
+	render(){
+
+		
+		return(
+				<div className='container'>
+					<h1 className'slogan'>Find someone to jam with</h1>
+					<div className='row'>
+						<Menu/>
+					</div>
+					<div className='row'>
+						{this.props.children}
+					</div>					
+				</div>
+
+			)
+	}
 }
-
-app.listen(port)
-console.log(`Listening at http://localhost:${port}`)
